c170e0210888016a735676a4176e54d746f81c3d
mattperkins <github@mattperkins.com>
2018-08-25 13:58:40 +0100

    add state

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import * as React from 'react'
 
 class App extends React.Component{
+ state={
+  ade: [
+   {id: 1, content: 'compile source code'},
+   {id: 2, content: 'administer repo boundary'}
+  ]
+ }
  render(){
   return(
    <h1>Add, delete, edit</h1>
